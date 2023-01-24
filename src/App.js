5c6a1abee074b62e3390c23ae4b75de973c0b45a
muratavci05 <js.muratavci@gmail.com>
2023-01-24 14:47:13 +0300

    components for material hooks

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-import { makeStyles } from "@material-ui/core";
-import Homepage from "./Pages/HomePage";
-import "./App.css";
+import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import CoinPage from "./Pages/CoinPage";
-import Header from "./components/Header";
+import "./App.css";
 
-const useStyles = makeStyles(() => ({
-  App: {
-    backgroundColor: "#14161a",
-    color: "white",
-    minHeight: "100vh",
-  },
-}));
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import HomePage from "./pages/HomePage";
+import CoinPage from "./pages/CoinPage";
+import { makeStyles } from "@material-ui/core";
 
 function App() {
+  const useStyles = makeStyles(() => ({
+    App: {
+      backgroundColor: "#14161a",
+      color: "white",
+      minHeight: "100vh",
+    },
+  }));
+
   const classes = useStyles();
 
   return (
     <BrowserRouter>
       <div className={classes.App}>
         <Header />
-        <Route path="/" component={Homepage} exact />
-        <Route path="/coins/:id" component={CoinPage} exact />
+        <Route path="/" component={HomePage} />
+        <Route path="/coins/:id" component={CoinPage} />
+        <Footer />
       </div>
     </BrowserRouter>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+
+import {
+  AppBar,
+  Typography,
+  Container,
+  Toolbar,
+  Select,
+  MenuItem,
+  makeStyles,
+  createTheme,
+  ThemeProvider,
+} from "@material-ui/core";
+
+const useStyles = makeStyles(() => ({
+  title: {
+    flex: 1,
+    color: "gold",
+    fontFamily: "Montserrat",
+    fontWeight: "bold",
+    cursor: "pointer",
+  },
+}));
+
+const Header = () => {
+  const classes = useStyles();
+  const history = useHistory();
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+
+      },
+      type: "dark",
+    }
+  })
+  return (
+    <ThemeProvider theme={darkTheme}>
+    <AppBar color="transparent" position="static">
+      <Container>
+        <Toolbar>
+          <Typography 
+          onClick={()=> history.push("/")}
+          className={classes.title}
+          variant="h6"
+          >Cryptocurrency APP
+          </Typography>
+          <Select
+            variant="outlined"
+            style={{
+              width: 100,
+              height: 40,
+              marginLeft: 15,
+            }}
+          >
+            <MenuItem value={"USD"}>USD</MenuItem>
+            <MenuItem value={"TRY"}>TRY</MenuItem>
+            <MenuItem value={"EUR"}>EUR</MenuItem>
+          </Select>
+        </Toolbar>
+      </Container>
+    </AppBar>
+    </ThemeProvider>
+  );
+};
+
+export default Header;
