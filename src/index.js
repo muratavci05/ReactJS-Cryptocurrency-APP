5fa19b0261933d8063a76ed96472fbf6787549ba
muratavci05 <js.muratavci@gmail.com>
2023-01-25 00:15:10 +0300

    carousel css add

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import CryptoContext from "./CryptoContext.jsx";
+import "react-alice-carousel/lib/alice-carousel.css";
 import "./index.css";
 
 ReactDOM.render(
