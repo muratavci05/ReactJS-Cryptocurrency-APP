edfe43bf37d5ac2cf984a73c222388a9a148c763
muratavci05 <js.muratavci@gmail.com>
2023-01-25 13:06:49 +0300

    banner, carousel files modified

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,61 +1,58 @@
-import { makeStyles,Container, Typography } from '@material-ui/core';
-import React from 'react'
-import Carousel from './Carousel';
+import { makeStyles, Container, Typography } from "@material-ui/core";
+import React from "react";
+import Carousel from "./Carousel";
 
-
-const useStyles = makeStyles (()=> ({
-    banner: {
-        backgroundImage: "url(./banner2.jpg)",
-    },
-    bannerContent: {
-        height:400,
-        display: "flex",
-        flexDirection:"column",
-        paddingTop:25,
-        justifyContent:"space-around",
-    },
-    tagline: {
-        display:"flex",
-        height: "40%",
-        flexDirection:"column",
-        justifyContent:"center",
-        textAlign:"center",
-    }
+const useStyles = makeStyles(() => ({
+  banner: {
+    backgroundImage: "url(./banner2.jpg)",
+  },
+  bannerContent: {
+    height: 400,
+    display: "flex",
+    flexDirection: "column",
+    paddingTop: 25,
+    justifyContent: "space-around",
+  },
+  tagline: {
+    display: "flex",
+    height: "40%",
+    flexDirection: "column",
+    justifyContent: "center",
+    textAlign: "center",
+  },
 }));
 
-
 const Banner = () => {
-
-    const classes = useStyles ();
+  const classes = useStyles();
   return (
     <div className={classes.banner}>
-        <Container className={classes.bannerContent}>
-            <div className={classes.tagline}>
-                <Typography
-                    variant='h2'
-                    style={{
-                        fontWeight:"bold",
-                        marginBottom:"15",
-                        fontFamily:"Montserrat",
-                    }}
-                >
-                        CryptoCurrency APP
-                </Typography>
-                <Typography
-                    variant='subtitle2'
-                    style={{
-                        Color:"darkgrey",
-                        fontFamily:"Montserrat",
-                        textTransform:"capitalize",
-                    }}
-                >
-Get all the Info regarding your favorite crypto Currency
-</Typography>
-            </div>
-            <Carousel/>
-        </Container>
+      <Container className={classes.bannerContent}>
+        <div className={classes.tagline}>
+          <Typography
+            variant="h2"
+            style={{
+              fontWeight: "bold",
+              marginBottom: "15",
+              fontFamily: "Montserrat",
+            }}
+          >
+            CryptoCurrency APP
+          </Typography>
+          <Typography
+            variant="subtitle2"
+            style={{
+              Color: "darkgrey",
+              fontFamily: "Montserrat",
+              textTransform: "capitalize",
+            }}
+          >
+            Get all the Info regarding your favorite crypto Currency
+          </Typography>
         </div>
-  )
-}
+        <Carousel />
+      </Container>
+    </div>
+  );
+};
 
 export default Banner;
--- src/components/Banner/Carousel.jsx
+++ src/components/Banner/Carousel.jsx
@@ -11,49 +11,67 @@ const useStyles = makeStyles((theme) => ({
     height: "50%",
     display: "flex",
     alignItems: "center",
+    backgroundColor: "transparent",
+    //backgroundImage:"linear-gradient(90deg, rgba(27,26,48,1) 0%, rgba(0,0,0,0.2049413515406162) 100%, rgba(14,36,41,0.7651654411764706) 100%);",
   },
 }));
+
+export function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3}+(?!\d))/g, ",");
+}
+
 const Carousel = () => {
   const [trending, setTrending] = useState([]);
 
   const classes = useStyles();
-  const { currency } = CryptoState();
+  const { currency, symbol } = CryptoState();
 
   const fetchTrendingCoins = async () => {
     const { data } = await axios.get(TrendingCoins(currency));
     setTrending(data);
   };
-  console.log (trending);
+  console.log(trending);
 
   useEffect(() => {
-        fetchTrendingCoins();
+    fetchTrendingCoins();
   }, [currency]);
 
   const items = trending.map((coin) => {
-    return(
-        <Link className={classes.carouselItem}
-        to={`/coins/${coin.id}`}>
-<img 
-src={coin?.image}
-alt={coin.name}
-height="80"
-style={{marginBottom:10}}
-/>
-        </Link>
-    )
-  })
+    let profit = coin.price_change_percentage_24h >= 0;
+    return (
+      <Link className={classes.carouselItem} to={`/coins/${coin.id}`}>
+        <img
+          src={coin?.image}
+          alt={coin.name}
+          height="80"
+          style={{ marginBottom: 10 }}
+        />
+        <span>
+          {coin?.symbol}
+          &nbsp;
+          <span>
+            {profit && "+"}
+            {coin?.price_change_percentage_24h?.toFixed(2)}%
+          </span>
+        </span>
+        <span style={{ fontSize: 22, fontWeight: 500 }}>
+          {symbol} {numberWithCommas(coin?.current_price.toFixed(2))}
+        </span>
+      </Link>
+    );
+  });
 
   const responsive = {
     0: {
-        items: 2,
+      items: 2,
     },
     512: {
-        items: 4,
+      items: 4,
     },
   };
   return (
     <div className={classes.carousel}>
-        <AliceCarousel
+      <AliceCarousel
         mouseTracking
         infinite
         autoPlayInterval={1000}
@@ -62,13 +80,9 @@ style={{marginBottom:10}}
         responsive={responsive}
         autoPlay
         items={items}
-        />
-            
-       
-
+      />
     </div>
-  )
-  
+  );
 };
 
 export default Carousel;
