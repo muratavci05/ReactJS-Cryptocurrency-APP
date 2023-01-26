e6104a9731f7f6a0a2ee12d60b1d1edca9432e9e
muratavci05 <js.muratavci@gmail.com>
2023-01-26 11:46:07 +0300

    coinpage file add

--- src/pages/CoinPage.jsx
+++ src/pages/CoinPage.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { makeStyles } from "@material-ui/core";
+import axios from "axios";
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { SingleCoin } from "../config/api";
 
 const CoinPage = () => {
+  const { id } = useParams();
+  const [coin, setCoins] = useState();
+
+  const { currency, symbol } = CryptoState();
+
+  const fetchCoin = async () => {
+    const { data } = await axios.get(SingleCoin(id));
+    setCoins(data);
+  };
+
+  useEffect(() => {
+    fetchCoin();
+  }, []);
+
+  const useStyles = makeStyles(() => ({}));
+  const classes = useStyles();
+
   return (
-    <div>CoinPage</div>
-  )
-}
+    <div className={classes.container}>
+      <div className={classes.sidebar}>{/*sidebar*/}</div>
+      {/*chart*/}
+    </div>
+  );
+};
 
 export default CoinPage;
