34d42c5b5795c81b6388764c66c6994e88acf85b
muratavci05 <js.muratavci@gmail.com>
2023-01-29 22:55:23 +0300

    coininfo pm modified

--- src/components/CoinInfo.jsx
+++ src/components/CoinInfo.jsx
@@ -75,7 +75,7 @@ const CoinInfo = ({ coin }) => {
                   let time =
                     date.getHours() > 12
                       //? `${date.getHours() - 12}:${date.getMinutes()} PM`
-                      ? `${date.getHours()+12}:${date.getMinutes()} PM`
+                      ? `${date.getHours()}:${date.getMinutes()} PM`
                       : `${date.getHours()}:${date.getMinutes()} AM`;
                   return days === 1 ? time : date.toLocaleDateString();
                 }),
